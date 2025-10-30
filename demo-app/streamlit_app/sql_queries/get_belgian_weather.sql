fdc54384d501b49e3e54bdbe2055fe30c73e4562
Pieter Huycke <pieter.huycke@dataminded.com>
2025-10-30 15:08:04 +0100

    Fix format of BQ query

--- demo-app/streamlit_app/sql_queries/get_belgian_weather.sql
+++ demo-app/streamlit_app/sql_queries/get_belgian_weather.sql
@@ -1,4 +1,3 @@
-  -- This file contains the SQL logic to fetch Belgian weather data.
 WITH
   WEATHER_DATA AS (
   SELECT
@@ -6,10 +5,12 @@ WITH
     -- Convert string date to actual DATE object
     PARSE_DATE('%Y%m%d', CONCAT(year, mo, da)) AS date,
     -- Handle missing data flags (9999.9 or 99.99)
-    -- Temperatures in Fahrenheit for now
+    -- All temperatures in Fahrenheit for now
     NULLIF(TEMP, 9999.9) AS avg_temp_f,
     NULLIF(max, 9999.9) AS max_temp_f,
-    NULLIF(min, 9999.9) AS min_temp_f
+    NULLIF(min, 9999.9) AS min_temp_f,
+    -- Rain in inches for now
+    NULLIF(prcp, 99.99) AS precipitation_in
   FROM
     `bigquery-public-data.noaa_gsod.gsod2025` ),
   BELGIAN_STATION_DATA AS (
@@ -21,26 +22,34 @@ WITH
   FROM
     `bigquery-public-data.noaa_gsod.stations`
   WHERE
-    -- Only focus on Belgian weather data
-    country = 'BE' )
+    -- Only focua on Belgian weather data
+    country = 'BE' ),
+  RESULT AS (
+  SELECT
+    -- From STATIONS CTE
+    B.name AS station_name,
+    B.lat,
+    B.lon,
+    -- From WEATHER CTE
+    W.date,
+    -- Convert to easier to understand metrics (Celsius, and millimeter)
+    ROUND((W.avg_temp_f - 32) * 5 / 9, 1) AS avg_temp_celsius,
+    ROUND((W.max_temp_f - 32) * 5 / 9, 1) AS max_temp_celsius,
+    ROUND((W.min_temp_f - 32) * 5 / 9, 1) AS min_temp_celsius,
+    ROUND(W.precipitation_in * 25.4, 1) AS precipitation_mm
+  FROM
+    WEATHER_DATA AS W
+  INNER JOIN
+    BELGIAN_STATION_DATA AS B
+  ON
+    W.stn = B.usaf
+  WHERE
+    -- Remove days where no temperature measurements are available
+    W.avg_temp_f IS NOT NULL
+  ORDER BY
+    B.name,
+    W.date)
 SELECT
-  B.name AS station_name,
-  W.date,
-  -- Convert to easier to understand metrics (Celsius, millimeter and kilometer per hour)
-  ROUND((W.avg_temp_f - 32) * 5 / 9, 1) AS avg_temp_celsius,
-  ROUND((W.min_temp_f - 32) * 5 / 9, 1) AS min_temp_celsius,
-  ROUND((W.max_temp_f - 32) * 5 / 9, 1) AS max_temp_celsius,
-  B.lat,
-  B.lon
+  *
 FROM
-  WEATHER_DATA AS W
-INNER JOIN
-  BELGIAN_STATION_DATA AS B
-ON
-  W.stn = B.usaf
-WHERE
-  -- Remove days where no temperature measurements are available
-  W.avg_temp_f IS NOT NULL
-ORDER BY
-  B.name,
-  W.date;
+  RESULT
